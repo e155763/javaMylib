56640f8adcfbead3cd2c3a81959375ab89d83b05
Motoki Maruyama <e155763@ie.u-ryukyu.ac.jp>
2018-01-30 22:30:06 +0900

    canny法の完成 細かな修正

--- src/ImageProcessing/Filter.java
+++ src/ImageProcessing/Filter.java
@@ -4,7 +4,6 @@ package ImageProcessing;
  * Created by e155763 on 2018/01/03.
  */
 
-import java.awt.Color;
 import java.util.Arrays;
 
 
@@ -15,22 +14,12 @@ public class Filter {
     private int extraPix; //どのくらい画像を拡大したか
     private int filterScale = 3; //フィルタの大きさ
 
-    private double Kernel[]; //空間フィルタを格納する変数
-    private int KernelPix[]; //フィルタをかけるピクセルの集合
-    private double KernelPixD[];
+    private double kernel[]; //空間フィルタを格納する変数
+    private int kernelPix[]; //フィルタをかけるピクセルの集合
     private int extendPix[]; //フィルタをかけるために拡大したピクセル
     private double extendPixD[];
-    private Color pixColor; //色情報取得
-    private double red = 0;
-    private double green = 0;
-    private double blue = 0;
+    private Color pixColor = new Color(); //色情報取得
     private double value[]; // grayscale用の配列
-    private double tmpRed = 0;
-    private double tmpGreen = 0;
-    private double tmpBlue = 0;
-
-    private final int BLACK = 0xFF000000;
-    private final int WHITE = 0xFFFFFFFF;
 
     public Filter(int width, int height){
         this.w = width;
@@ -75,23 +64,23 @@ public class Filter {
 
     // 畳み込まれるピクセルを取得
     private void setConvolutedKernel(int x, int y) {
-        KernelPix = new int[filterScale * filterScale];
+        kernelPix = new int[filterScale * filterScale];
         int k = 0;
         for (int i = -extraPix; i <= extraPix; i++) {
             for (int j = -extraPix; j <= extraPix; j++) {
-                KernelPix[k] = extendPix[getExtendPixPoint(x+i, y+j)];
+                kernelPix[k] = extendPix[getExtendPixPoint(x+i, y+j)];
                 k++;
             }
         }
     }
 
     // canny法用
-    private void setConvolutedKernelD(int x, int y) {
-        KernelPixD = new double[filterScale * filterScale];
+    private void setKernelD(int x, int y) {
+        kernel = new double[filterScale * filterScale];
         int k = 0;
         for (int i = -extraPix; i <= extraPix; i++) {
             for (int j = -extraPix; j <= extraPix; j++) {
-                KernelPixD[k] = extendPixD[getExtendPixPoint(x+i, y+j)];
+                kernel[k] = extendPixD[getExtendPixPoint(x+i, y+j)];
                 k++;
             }
         }
@@ -148,14 +137,23 @@ public class Filter {
 
     // フィルタをかけるメソッド
     private void convolution(int pix[], double filter[]) {
+        double red;
+        double green;
+        double blue;
+        double tmpRed;
+        double tmpGreen;
+        double tmpBlue;
+
         for (int x = 0; x < w; x++) {
             for (int y = 0; y < h; y++) {
+                tmpRed = 0;
+                tmpGreen = 0;
+                tmpBlue = 0;
                 setConvolutedKernel(x, y);
                 for (int i = 0; i < filterScale * filterScale; i++) {
-                    pixColor = new Color(KernelPix[i]);
-                    red = pixColor.getRed();
-                    green = pixColor.getGreen();
-                    blue = pixColor.getBlue();
+                    red = pixColor.getRed(kernelPix[i]);
+                    green = pixColor.getGreen(kernelPix[i]);
+                    blue = pixColor.getBlue(kernelPix[i]);
                     tmpRed += red * filter[i];
                     tmpGreen += green * filter[i];
                     tmpBlue += blue * filter[i];
@@ -165,35 +163,31 @@ public class Filter {
                 green = rounding(tmpGreen);
                 blue = rounding(tmpBlue);
                 if(pix != null) {
-                    pixColor = new Color((int) red, (int) green, (int) blue);
-                    pix[getPixPoint(x, y)] = pixColor.getRGB(); //平均を取る
+                    pix[getPixPoint(x, y)] = pixColor.getRGB((int) red, (int) green, (int) blue); //平均を取る
                 }else{
                     value[getPixPoint(x, y)] = tmpRed;
                 }
-                tmpRed = 0;
-                tmpGreen = 0;
-                tmpBlue = 0;
             }
         }
     }
 
     // 平均化フィルタ
-    public void averagingFilter(int pix[], int filterScale) {
+    public void averagingFilter(int pix[], int newPix[], int filterScale) {
         this.filterScale = filterScale;
         this.extraPix = (1 + filterScale) / 2 - 1;
-        this.Kernel = new double [filterScale * filterScale];
+        this.kernel = new double [filterScale * filterScale];
         int ave = filterScale * filterScale;
-        Arrays.fill(Kernel, 1 / ave);
+        Arrays.fill(kernel, 1 / ave);
 
         init(pix);
-        convolution(pix, Kernel);
+        convolution(newPix, kernel);
     }
 
     // ガウシアンフィルタ
     public void gaussianFilter(int pix[],int newPix[], int filterScale, double sigma) {
         this.filterScale = filterScale;
         this.extraPix = (1 + filterScale) / 2 - 1;
-        this.Kernel = new double[filterScale * filterScale];
+        this.kernel = new double[filterScale * filterScale];
         double normalize = 0.0;
 
         init(pix);
@@ -201,32 +195,32 @@ public class Filter {
         //フィルタの作成
         for (int x = 0; x < filterScale; x++) {
             for (int y = 0; y < filterScale; y++) {
-                Kernel[x * filterScale + y] = Math.exp(-(Math.pow(x - extraPix, 2.0) + Math.pow(y - extraPix, 2.0)) / 2 * Math.pow(sigma, 2.0)) / 2.0 * Math.PI * Math.pow(sigma, 2.0);
-                normalize += Kernel[x * filterScale + y];
+                kernel[x * filterScale + y] = Math.exp(-(Math.pow(x - extraPix, 2.0) + Math.pow(y - extraPix, 2.0)) / 2 * Math.pow(sigma, 2.0)) / 2.0 * Math.PI * Math.pow(sigma, 2.0);
+                normalize += kernel[x * filterScale + y];
             }
         }
         for(int i = 0; i < filterScale * filterScale; i++){
-            Kernel[i] /= normalize;
+            kernel[i] /= normalize;
         }
-        convolution(newPix, Kernel);
+        convolution(newPix, kernel);
     }
 
     // ソーベルフィルタ X
     public void sobelFilterX(int pix[],int newPix[]) {
         this.filterScale = 3;
         this.extraPix = (1 + filterScale) / 2 - 1;
-        this.Kernel = new double[]{-1, 0, 1, -2, 0, 2, -1, 0, 1};
+        this.kernel = new double[]{-1, 0, 1, -2, 0, 2, -1, 0, 1};
         init(pix);
-        convolution(newPix, Kernel);
+        convolution(newPix, kernel);
     }
 
     // ソーベルフィルタ Y
     public void sobelFilterY(int pix[],int newPix[]) {
         this.filterScale = 3;
         this.extraPix = (1 + filterScale) / 2 - 1;
-        this.Kernel = new double[]{1, 2, 1, 0, 0, 0, -1, -2, -1};
+        this.kernel = new double[]{1, 2, 1, 0, 0, 0, -1, -2, -1};
         init(pix);
-        convolution(newPix, Kernel);
+        convolution(newPix, kernel);
     }
 
 
@@ -244,7 +238,7 @@ public class Filter {
 
         sh.toGrayScale(pix, w, h);
 
-        gaussianFilter(pix,pix, 5, sigma);
+        gaussianFilter(pix, pix, 5, sigma);
 
         sobelFilterX(pix,null);
         System.arraycopy(value, 0, fx, 0, pix.length);
@@ -253,9 +247,8 @@ public class Filter {
 
         for(int i = 0; i < h * w; i++) {
             g[i] = (Math.sqrt(Math.pow(fx[i], 2) + Math.pow(fy[i], 2)));
-            d[i] = Math.tan(fy[i] / fx[i]);
-            pixColor = new Color(rounding(g[i]), rounding(g[i]), rounding(g[i]));
-            newPix[i] = pixColor.getRGB();
+            d[i] = fy[i] / fx[i];
+            newPix[i] = pixColor.getGRAY(rounding(g[i]));
         }
 
         // Non-maximum Suppression
@@ -265,24 +258,24 @@ public class Filter {
         init(g);
         for(int x = 0; x < w; x++){
             for(int y = 0; y < h; y++){
-                setConvolutedKernelD(x, y);
+                setKernelD(x, y);
                 theta = d[getPixPoint(x, y)];
 
                 if (-0.4142 < theta && theta <= 0.4142){
-                    if(KernelPixD[4] <= KernelPixD[3] || KernelPixD[4] <= KernelPixD[5]){
-                        newPix[getPixPoint(x,y)] = BLACK;
+                    if(kernel[4] <= kernel[3] || kernel[4] <= kernel[5]){
+                        newPix[getPixPoint(x,y)] = pixColor.BLACK;
                     }
                 }else if(0.4142 < theta && theta < 2.4142){
-                    if(KernelPixD[4] <= KernelPixD[2] || KernelPixD[4] <= KernelPixD[6]){
-                        newPix[getPixPoint(x,y)] = BLACK;
+                    if(kernel[4] <= kernel[2] || kernel[4] <= kernel[6]){
+                        newPix[getPixPoint(x,y)] = pixColor.BLACK;
                     }
                 }else if( Math.abs(theta) >= 2.4142){
-                    if(KernelPixD[4] <= KernelPixD[1] || KernelPixD[4] <= KernelPixD[7]){
-                        newPix[getPixPoint(x,y)] = BLACK;
+                    if(kernel[4] <= kernel[1] || kernel[4] <= kernel[7]){
+                        newPix[getPixPoint(x,y)] = pixColor.BLACK;
                     }
                 }else if(-2.4142 < theta && theta < -0.4142){
-                    if(KernelPixD[4] <= KernelPixD[0] || KernelPixD[4] <= KernelPixD[8]){
-                        newPix[getPixPoint(x,y)] = BLACK;
+                    if(kernel[4] <= kernel[0] || kernel[4] <= kernel[8]){
+                        newPix[getPixPoint(x,y)] = pixColor.BLACK;
                     }
                 }
             }
@@ -294,23 +287,20 @@ public class Filter {
         for(int x = 0; x < w; x++) {
             for (int y = 0; y < h; y++) {
                 setConvolutedKernel(x, y);
-                pixColor = new Color(KernelPix[4]);
-                center = pixColor.getRed();
+                center = pixColor.getGray(kernelPix[4]);
                 if(thHigh <= center){
-                    newPix[getPixPoint(x,y)] = WHITE;
+                    newPix[getPixPoint(x,y)] = pixColor.WHITE;
                 }else if(thLow < center && center < thHigh){
-                    for (int aKernelPix : KernelPix) {
-                        pixColor = new Color(aKernelPix);
-                        if (thHigh <= pixColor.getRed()) {
-                            newPix[getPixPoint(x, y)] = WHITE;
+                    for (int aKernelPix : kernelPix) {
+                        if (thHigh <= pixColor.getRed(aKernelPix)) {
+                            newPix[getPixPoint(x, y)] = pixColor.BLACK;
                             break;
                         }
-                        newPix[getPixPoint(x, y)] = BLACK;
+                        newPix[getPixPoint(x, y)] = pixColor.BLACK;
                     }
                 }else if(center <= thLow){
-                    newPix[getPixPoint(x,y)] = BLACK;
+                    newPix[getPixPoint(x,y)] = pixColor.BLACK;
                 }
-
             }
         }
     }
